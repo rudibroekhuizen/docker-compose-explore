9e32d1881edaac776237f76295263ac00d8f89f3
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2020-11-22 08:58:56 +0100

    gbif.json

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # create indices with mappings
-curl -i -X PUT -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:9200/users -d @mappings/users.json
+curl -i -X PUT -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:9200/users -d @mappings/gbif.json
 
 # setup connections
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @connectors/psql_connector.json
