ff2442474750e740bf1ba69d6c77d7bc3b9ba302
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2020-12-11 21:52:52 +0100

    Update and rename start.sh to mapping_connector.sh

--- mapping_connector.sh
+++ mapping_connector.sh
@@ -1,6 +1,6 @@
 # create indices with mappings
 curl -i -X PUT -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:9200/db1.explore.gbif -d @mappings/gbif.json
 
-# setup connections
+# setup connectors
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @connectors/psql_connector.json
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @connectors/es_connector.json
