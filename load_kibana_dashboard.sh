139f462b957a7b1654f0d1c3969e8c913e085368
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2020-12-12 21:59:25 +0100

    Update load_kibana_dashboard.sh

--- load_kibana_dashboard.sh
+++ load_kibana_dashboard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-echo -e "\n--\n+> Load Kibana dashboard"
+echo -e "Load Kibana dashboard"
 curl -X POST "localhost:5601/api/saved_objects/_import" -H 'kbn-xsrf: true' --form file=@kibana/explore.ndjson
