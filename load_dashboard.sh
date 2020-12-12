a76083633229b370962492c7a7b22b7e7e0cd0c2
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2020-12-12 12:49:41 +0100

    Rename

--- load_dashboard.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo -e "\n--\n+> Load Kibana dashboard"
-curl -X POST "localhost:5601/api/saved_objects/_import" -H 'kbn-xsrf: true' --form file=@kibana/explore.ndjson
