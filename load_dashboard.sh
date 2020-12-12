2098b4685faf66049fd4598559cf68de06c2fe70
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2020-12-12 12:27:30 +0100

    Update load_dashboard.sh

--- load_dashboard.sh
+++ load_dashboard.sh
@@ -1,2 +1,4 @@
-echo -e "\n--\n+> Setup Kibana Dashboard"
-curl -X POST "localhost:5601/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@/scripts/export.json
+#!/bin/bash
+
+echo -e "\n--\n+> Load Kibana dashboard"
+curl -X POST "localhost:5601/api/saved_objects/_import" -H 'kbn-xsrf: true' --form file=@kibana/explore.ndjson
