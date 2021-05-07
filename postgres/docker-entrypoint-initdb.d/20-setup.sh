059d27e2a9512dd4a83365bff8081e19f887ced4
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2021-05-07 10:27:58 +0200

    Update 20-setup.sh

--- postgres/docker-entrypoint-initdb.d/20-setup.sh
+++ postgres/docker-entrypoint-initdb.d/20-setup.sh
@@ -1,9 +1,28 @@
 #/bin/bash
 set -e
 
-wget -P /tmp https://api.gbif.org/v1/occurrence/download/request/0107080-200613084148143.zip
-unzip /tmp/0107080-200613084148143.zip -d /tmp
+wget -P /tmp https://api.gbif.org/v1/occurrence/download/request/0266669-200613084148143.zip
+unzip /tmp/0266669-200613084148143.zip -d /tmp
 
 awk 'NR==1{$0=tolower($0)} 1' /tmp/occurrence.txt > /tmp/occurrence.tsv
 
 tail -n +2 /tmp/occurrence.tsv > /tmp/gbif_raw.tsv
+
+sed -i -e 215166d /tmp/gbif_raw.tsv
+sed -i -e 955221d /tmp/gbif_raw.tsv
+sed -i -e 1449592d /tmp/gbif_raw.tsv
+sed -i -e 1578092d /tmp/gbif_raw.tsv
+sed -i -e 2041117d /tmp/gbif_raw.tsv
+sed -i -e 2090923d /tmp/gbif_raw.tsv
+sed -i -e 2274063d /tmp/gbif_raw.tsv
+sed -i -e 2948274d /tmp/gbif_raw.tsv
+sed -i -e 2964065d /tmp/gbif_raw.tsv
+sed -i -e 3216290d /tmp/gbif_raw.tsv
+sed -i -e 3406478d /tmp/gbif_raw.tsv
+sed -i -e 3852303d /tmp/gbif_raw.tsv
+sed -i -e 4216860d /tmp/gbif_raw.tsv
+sed -i -e 4223270d /tmp/gbif_raw.tsv
+sed -i -e 4361782d /tmp/gbif_raw.tsv
+sed -i -e 4459671d /tmp/gbif_raw.tsv
+sed -i -e 4517094d /tmp/gbif_raw.tsv
+sed -i -e 4778815d /tmp/gbif_raw.tsv
