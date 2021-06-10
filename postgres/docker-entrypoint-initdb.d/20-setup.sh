2044460b4e42e7d1d067a0c67b582bc4d573e732
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2021-06-10 12:07:13 +0200

    Update 20-setup.sh

--- postgres/docker-entrypoint-initdb.d/20-setup.sh
+++ postgres/docker-entrypoint-initdb.d/20-setup.sh
@@ -4,6 +4,5 @@ set -e
 wget -P /tmp https://api.gbif.org/v1/occurrence/download/request/0107080-200613084148143.zip
 unzip /tmp/0107080-200613084148143.zip -d /tmp
 
-awk 'NR==1{$0=tolower($0)} 1' /tmp/occurrence.txt > /tmp/occurrence.tsv
-
-tail -n +2 /tmp/occurrence.tsv > /tmp/gbif_raw.tsv
+# Remove headerline from occurence.txt
+tail -n +2 /tmp/occurrence.txt > /tmp/gbif_raw.tsv
