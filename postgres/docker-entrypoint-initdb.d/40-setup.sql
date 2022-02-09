04b6f16ae1757985da6856abdf42c9be7afcea19
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2022-02-09 21:11:07 +0100

    Rm select *

--- postgres/docker-entrypoint-initdb.d/40-setup.sql
+++ postgres/docker-entrypoint-initdb.d/40-setup.sql
@@ -6,21 +6,19 @@ CREATE EXTENSION IF NOT EXISTS topn;
 -- Create enriched table
 CREATE TABLE explore.gbif_enriched AS 
 WITH a AS (
-SELECT * FROM explore.gbif 
-), b AS (	
 SELECT *,
 make_date((EXTRACT (YEAR FROM eventdate))::int, (EXTRACT (MONTH FROM eventdate))::int, '1') as time,
 ST_ClusterKMeans(geom, 500) OVER() AS cluster_id
-FROM a
-), c AS (
+FROM explore.gbif
+), b AS (
 SELECT *, 
 ST_UNION(geom) OVER (PARTITION BY cluster_id ORDER BY cluster_id) AS stunion 
-FROM b
+FROM a
 )
 SELECT *, 
 ST_SetSRID(ST_Centroid(stunion), 4326) AS centro_id,
 ST_Concavehull(ST_RemoveRepeatedPoints(stunion,1),1) AS concave
-FROM c;
+FROM b;
 
 -- Create aggregation table
 CREATE TABLE explore.gbif_aggregated(
